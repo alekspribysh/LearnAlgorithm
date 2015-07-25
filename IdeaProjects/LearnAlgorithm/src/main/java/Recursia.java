bb721a9fb5faec6a0f945cf0eae792a0800e02b2
alekspribysh <alekspribysh@gmail.com>
2015-07-24 22:42:40 -0700

    first commit

---
+++ IdeaProjects/LearnAlgorithm/src/main/java/Recursia.java
@@ -0,0 +1,120 @@
+/**
+ * Created by aprybysh on 9/4/15.
+ */
+public class Recursia {
+
+    public static void main(String[] args) {
+        int n = 212;
+        int k = 68;
+
+        int[] array = {11, 12, 1, 11, 3, 6, 5, 4, 11};
+        int index = 0;
+        int le = array.length;
+
+        //System.out.println(factorial(n));
+        //System.out.println(febonachi(n));
+        // System.out.println(gcd2(n, k));
+        //System.out.println(ears(n));
+        // System.out.println(count7(n));
+        System.out.println(array11(array, index));
+
+
+    }
+
+
+    public static int factorial(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int pred = factorial(n - 1);
+        int cur = pred * n;
+
+
+        return cur;
+        //return factorial(n-1)*n;
+    }
+
+    public static int febonachi(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n == 2) {
+            return 1;
+        }
+//        int feb1 = febonachi(n - 1);
+//        int feb2 = febonachi(n - 2);
+
+        int feb = febonachi(n - 1) + febonachi(n - 2);
+
+        //return febonachi(n-1)+febonachi(n-2);
+        return feb;
+    }
+
+
+    public static int gcd2(int p, int q) {
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+
+    public static int ears(int n) {
+        if (n == 0) {
+            return n;
+        }
+
+        if (n % 2 == 0) {
+            return ears(n - 1) + 3;
+        } else {
+            return ears(n - 1) + 2;
+        }
+
+    }
+
+    public static int count7(int n) {
+        int c = 0;
+        if (n < 7) {
+            return 0;
+        } else {
+            if (n % 10 == 7) {
+                c = 1;
+            } else {
+                c = 0;
+            }
+        }
+        return c + count7(n / 10);
+
+    }
+
+
+
+    public static int array11(int[] array, int index) {
+
+
+        if (array.length  != 0) {
+            return 0;
+        }
+
+        int count = array11(array, index + 1);
+
+        if (array[index] == 11) {
+            count = count + 1;
+            array.
+
+        }
+
+
+        return count;
+
+
+    }
+
+}
+
+
+
